3159243c858586fda4d33261499cafe5fcdb66af
Gorchanyuk <gorchanyk@gmail.com>
2024-05-25 02:21:47 +0400

    Переписал сваггер на аннотациях

--- src/main/java/ru/gorchanyuk/securitywithjwt/config/SwaggerConfiguration.java
+++ src/main/java/ru/gorchanyuk/securitywithjwt/config/SwaggerConfiguration.java
@@ -1,39 +1,28 @@
 package ru.gorchanyuk.securitywithjwt.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class SwaggerConfiguration {
-    final String securitySchemeName = "bearerAuth";
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
-    @Bean
-    public OpenAPI openAPI() {
-
-        return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                .components(
-                        new Components()
-                                .addSecuritySchemes(securitySchemeName,
-                                        new SecurityScheme()
-                                                .name(securitySchemeName)
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")
-                                )
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Spring Security with JWT",
+                description = "Предоставляет демонстрационные методы для работы с сервисом SecurityWithJWT с использованием JWT токена",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Gorchanyuk"
                 )
-                .info(new Info()
-                        .title("Spring Security with JWT")
-                        .description("Предоставляет демонстрационные методы для работы с сервисом SecurityWithJWT с использованием JWT токена")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Gorchanyuk")
-                                .email("gorchanuk@gmail.com")));
-    }
+        ),
+        security = {@SecurityRequirement(name = "Bearer Authentication")}
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer")
+public class SwaggerConfiguration {
 }
